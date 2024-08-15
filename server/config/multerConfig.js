da67e110e6e66d54602924a100d6e082502200e3
Paul Ivan Dela Cruz <kmpkhai001@gmail.com>
2024-08-15 23:34:05 +0800

    "first commit"

---
+++ server/config/multerConfig.js
@@ -0,0 +1,31 @@
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../uploads/'));
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+export const fileFilter = (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only! (jpeg, jpg, png)');
+    }
+};
+
+export const limits = {
+  fileSize: 2 * 1024 * 1024,
+};
