0398aead4f930230f800238e5b67c48b3be784a1
Paul Ivan Dela Cruz <kmpkhai001@gmail.com>
2024-08-15 23:33:56 +0800

    "first commit"

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import userRoute from './routes/userRoute.js';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'));
+
+app.use('/api/users', userRoute);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Something went wrong!' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
